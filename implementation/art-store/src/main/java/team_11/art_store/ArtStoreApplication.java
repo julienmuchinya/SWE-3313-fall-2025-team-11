5d523aa6293a7d5ec0950590a4c893d4ec565488
Isaac Lai <64813126+isaaclai30@users.noreply.github.com>
2025-11-18 15:13:31 -0500

    Spring boot project added

---
+++ implementation/art-store/src/main/java/team_11/art_store/ArtStoreApplication.java
@@ -0,0 +1,13 @@
+package team_11.art_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtStoreApplication.class, args);
+	}
+
+}
