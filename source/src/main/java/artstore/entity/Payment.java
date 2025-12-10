c09e39d5ea7cd5cd2836ccaab7f1f9af53a50129
cooperparrish <cparrish03888@gmail.com>
2025-12-09 22:02:52 -0500

    Update Payment.java
    added column for shipping fee

--- source/src/main/java/artstore/entity/Payment.java
+++ source/src/main/java/artstore/entity/Payment.java
@@ -26,6 +26,7 @@ public class Payment {
     private PaymentMethod paymentMethod;
 
     private BigDecimal amount;
+    private BigDecimal shippingFee;
     private String status;
 
     @Column(columnDefinition = "DATETIME DEFAULT GETDATE()")
@@ -37,4 +38,5 @@ public class Payment {
             this.paidAt = LocalDateTime.now();
         }
     }
+
 }
